627d8d82e9b651bfcf49ea123d8ba144fa5f5949
ahmad shalan <engashalan@gmail.com>
2021-03-22 09:57:47 +0200

    strech

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -159,6 +159,25 @@ Return the resulting output array.
 
 const fizzbuzz = (arr) => {
   // Solution code here...
+  let output=[];
+
+  arr.forEach(item =>{
+
+    if (item % 3 === 0 && item % 5 === 0){
+
+      output.push('Fizz Buzz')
+    }else if (item % 3 === 0){
+
+      output.push('Fizz')
+    }else if (item % 5 === 0){
+
+      output.push('Buzz')
+    }else{
+      output.push(item)
+    }
+  });
+
+  return output;
 };
 
 /* ------------------------------------------------------------------------------------------------
